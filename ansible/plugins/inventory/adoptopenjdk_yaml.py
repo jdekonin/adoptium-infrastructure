b231b15abc5b979119c556e8b230bd9475c6566c
Brad Blondin <Brad_Blondin@ca.ibm.com>
2018-02-15 13:20:35 -0500

    Update adoptopenjdk_yaml.py

--- ansible/plugins/inventory/adoptopenjdk_yaml.py
+++ ansible/plugins/inventory/adoptopenjdk_yaml.py
@@ -37,17 +37,7 @@ import os
 import sys
 from os import path
 
-valid = {
-  # taken from nodejs/node.git: ./configure
-  'arch': ('armv7', 'armv8', 'ppc64le', 'ppc64', 'x64', 's390x'),
 
-  # valid roles - add as necessary
-  'type': ('build', 'test', 'jck'),
-
-  # providers - validated for consistency
-  'provider': ('cloudcone', 'joyent', 'marist', 'osuosl', 'scaleway',
-        'macstadium', 'macincloud', 'softlayer', 'packet', 'linaro', '1and1')
-}
 
 # customisation options per host:
 #
@@ -148,25 +138,6 @@ def main():
     print(json.dumps(export, indent=2))
 
 
-def parse_host(host):
-    """Parses a host and validates it against our naming conventions"""
-
-    hostinfo = dict()
-    info = host.split('-')
-
-    expected = ['type', 'provider', 'os', 'arch', 'uid']
-
-    if len(info) is not 5:
-        raise Exception('Host format is invalid: %s,' % host)
-
-    for key, item in enumerate(expected):
-        hostinfo[item] = has_metadata(info[key])
-
-    for item in ['type', 'provider', 'arch']:
-        if hostinfo[item] not in valid[item]:
-            raise Exception('Invalid %s: %s' % (item, hostinfo[item]))
-
-    return hostinfo
 
 
 def has_metadata(info):

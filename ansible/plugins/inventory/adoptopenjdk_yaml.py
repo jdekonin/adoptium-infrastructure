5a5c820c54c007b7eec736fe8ddb883e9da94cd4
Brad Blondin <Brad_Blondin@ca.ibm.com>
2018-02-15 14:15:20 -0500

    Update adoptopenjdk_yaml.py

--- ansible/plugins/inventory/adoptopenjdk_yaml.py
+++ ansible/plugins/inventory/adoptopenjdk_yaml.py
@@ -37,6 +37,17 @@ import os
 import sys
 from os import path
 
+valid = {
+  # taken from nodejs/node.git: ./configure
+  'arch': ('armv7', 'armv8', 'ppc64le', 'ppc64', 'x64', 's390x'),
+
+  # valid roles - add as necessary
+  'type': ('build', 'test', 'jck'),
+
+  # providers - validated for consistency
+  'provider': ('cloudcone', 'joyent', 'marist', 'osuosl', 'scaleway',
+        'macstadium', 'macincloud', 'softlayer', 'packet', 'linaro', '1and1')
+}
 
 # customisation options per host:
 #
@@ -83,7 +94,56 @@ def main():
                 'ansible_ssh_private_key_file': key
             }
 
+            for provider in providers:
+                for provider_name, hosts in provider.iteritems():
+                    for host, metadata in hosts.iteritems():
+                        # some hosts have metadata appended to provider
+                        # which requires underscore
+                        delimiter = "_" if host.count('-') is 3 else "-"
+                        hostname = '{}-{}{}{}'.format(host_type, provider_name,
+                                                      delimiter, host)
 
+                        export[host_type]['hosts'].append(hostname)
+
+                        c = {}
+
+                        try:
+                            parsed_host = parse_host(hostname)
+                            for k, v in parsed_host.iteritems():
+                                c.update({k: v[0] if type(v) is dict else v})
+                        except Exception, e:
+                            raise Exception('Failed to parse host: %s' % e)
+
+                        c.update({'ansible_host': metadata['ip']})
+
+                        if 'port' in metadata:
+                            c.update({'ansible_port': str(metadata['port'])})
+
+                        if 'user' in metadata:
+                            c.update({'ansible_user': metadata['user']})
+                            if 'win' not in hostname:
+                                c.update({'ansible_become': True})
+
+                        if 'labels' in metadata:
+                            c.update({'labels': metadata['labels']})
+
+                        if 'alias' in metadata:
+                            c.update({'alias': metadata['alias']})
+
+                        if 'win' in hostname:
+                            c.update({'is_win': True})
+
+                        # add specific options from config
+                        for option in ifilter(lambda s: s.startswith('hosts:'),
+                                              config.sections()):
+                            # remove `hosts:`
+                            if option[6:] in hostname:
+                                for o in config.items(option):
+                                    # configparser returns tuples of key, value
+                                    c.update({o[0]: o[1]})
+
+                        export['_meta']['hostvars'][hostname] = {}
+                        export['_meta']['hostvars'][hostname].update(c)
 
     print(json.dumps(export, indent=2))
 
@@ -92,6 +152,19 @@ def parse_host(host):
     """Parses a host and validates it against our naming conventions"""
 
     hostinfo = dict()
+    info = host.split('-')
+
+    expected = ['type', 'provider', 'os', 'arch', 'uid']
+
+    if len(info) is not 5:
+        raise Exception('Host format is invalid: %s,' % host)
+
+    for key, item in enumerate(expected):
+        hostinfo[item] = has_metadata(info[key])
+
+    for item in ['type', 'provider', 'arch']:
+        if hostinfo[item] not in valid[item]:
+            raise Exception('Invalid %s: %s' % (item, hostinfo[item]))
 
     return hostinfo
 
@@ -114,6 +187,10 @@ def has_metadata(info):
     return key if metadata else info
 
 
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', action='store_true')
+    parser.add_argument('--host', action='store')
+    args = parser.parse_args()
 
     main()
